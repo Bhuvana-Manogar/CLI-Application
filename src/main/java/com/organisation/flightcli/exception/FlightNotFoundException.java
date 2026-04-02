21ac8c96da563e02cde879cff023bcaed196d86b
Bhuvana <147125678+Bhuvana-Manogar@users.noreply.github.com>
2026-04-02 12:16:58 +0530

    java files

---
+++ src/main/java/com/organisation/flightcli/exception/FlightNotFoundException.java
@@ -0,0 +1,5 @@
+package com.organisation.flightcli.exception;
+
+public class FlightNotFoundException extends RuntimeException {
+    public FlightNotFoundException(String message) { super(message); }
+}
